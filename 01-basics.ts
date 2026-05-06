3d141ff18ec9f410bb54d4c113455841d549f298
Ruth May Regino <ruthmayreginos2786@gmail.com>
2026-05-06 11:02:40 +0800

    Done playing and updating the 01-basic.ts file

--- 01-basics.ts
+++ 01-basics.ts
@@ -1,11 +1,11 @@
 // console.log("Hello World!")
 
-let userName: string = "Elmer";
-let userAge = 31;
+let userName: string = "Ruth May";
+let userAge: number = 22;
 
 // ...
 
-userAge = "31"
+userAge = 22
 
 console.log(`Username - ${userName}`)
 console.log(`Age - ${userAge}`)
@@ -14,7 +14,7 @@ function add(a: number, b = 5) {
     return a + b
 }
 
-add(10)
+add(11)
 // add('10')
-add(10, 6)
+add(12, 10)
 // add(10, '6')
